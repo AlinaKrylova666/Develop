56513340aa01a20ba5616d0b086ce8a601565e53
Your Name <you@example.com>
2025-03-05 20:03:35 +0300

    Initial commit

--- src/main.py
+++ src/main.py
@@ -4,3 +4,13 @@ file_path = '../data/transactions.json'
 transactions = load_transactions(file_path)
 
 print(transactions)
+
+
+from decorators import log
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x + y
+
+if __name__ == "__main__":
+    print(my_function(1, 2))
